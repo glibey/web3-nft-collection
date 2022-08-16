1765df1aa4afd6da9d9b3b22b4dc0cc4b166c9a1
Sergey Glibey <sergey.glibey@gmail.com>
2022-08-16 12:27:52 +0300

    -- NFT Collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf3353cceeA10332d2Eb8eE362b3f279Ef7073817";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-theta-three.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
